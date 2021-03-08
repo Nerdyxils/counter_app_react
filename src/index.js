26e88e390a9f3ad67c8084ddb985d6d763e16930
Nerdyxils <silasabiodun25@gmail.com>
2021-03-08 14:57:22 +0100

    trying to fix deplo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
+ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+    </BrowserRouter>),
   document.getElementById('root')
 );
